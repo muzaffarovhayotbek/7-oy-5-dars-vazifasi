112c44eccfe7f05f4f18e6912aa251337f353484
muzaffarovhayotbek <hayotbek0511@gmail.com>
2024-12-23 20:46:04 +0500

    finish

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement } from '../store/CounterSlice';
 
 function Home() {
-  return <div>Home</div>;
+  const counter = useSelector((state) => state.counter);
+  console.log(counter);
+
+  const dispatch = useDispatch();
+
+  function handleIncrement() {
+    dispatch(increment(10));
+  }
+  function handleDecrement() {
+    dispatch(decrement(10));
+  }
+
+  return (
+    <div>
+      Home
+      <h3>{counter}</h3>
+      <button onClick={handleIncrement}>increment</button>
+      <button onClick={handleDecrement}>decrement</button>
+    </div>
+  );
 }
 
 export default Home;
