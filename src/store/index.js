1f5b4e65d85c0d75c46ec9f26c9c0d7fd768d54c
muzaffarovhayotbek <hayotbek0511@gmail.com>
2024-12-24 16:12:00 +0500

    add

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './CounterSlice';
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    // theme: themeReducer,
+    'counter': counterReducer,
   },
 });
